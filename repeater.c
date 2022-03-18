c0b31ae283c25a871bddc396186065d9be797f12
zi0821 <100885971+zi0821@users.noreply.github.com>
2022-03-18 09:11:15 +0800

    Create repeater.c

---
+++ repeater.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int d = 0;
+    int e = 0;
+
+    printf("每輸入一個數字，程式會再印出相同的數字。\n");
+    //fill some condition into the following while loop
+    //only ONE line code is you need
+scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
+    printf("a=%d\n", a);
+    printf("b=%d\n", b);
+    printf("c=%d\n", c);
+    printf("d=%d\n", d);
+    printf("e=%d\n", e);
+    }
